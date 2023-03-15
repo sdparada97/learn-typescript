003ee13c24e687d4e7119802f3fba70064dd8a5e
sdparada97 <sdparada97@gmail.com>
2023-03-15 11:28:19 -0500

    Hola mundo TSC

---
+++ bases/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const msg = 'Hola mundo TS';
+const hero = {
+    name: 'IRONMAN',
+    age: 45
+};
+hero.age = 50;
+console.log(hero.age + 1);
---
+++ bases/app.ts
@@ -0,0 +1,7 @@
+const msg:string = 'Hola mundo TS';
+const hero = {
+    name:'IRONMAN',
+    age:45
+}
+hero.age = 50
+console.log(hero.age + 1)
