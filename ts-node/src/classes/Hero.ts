d7d95cc74ee7db376645c24a832126836dd21d5e
sdparada97 <sdparada97@gmail.com>
2023-04-03 11:50:36 -0500

    Se agrega sección de NameSpaces en typescript

---
+++ ts-node/src/classes/Hero.ts
@@ -0,0 +1,14 @@
+import powers from '../data/powers'; 
+
+export class Hero {
+    constructor(
+        public name:string,
+        public powerId:number,
+        public age:number
+    ){}
+    
+    public get power() : string { 
+        return powers.find(power=> power.id === this.powerId)?.desc || 'not found'
+    }
+    
+}
---
+++ ts-node/src/index.ts
@@ -0,0 +1,14 @@
+// Exports General or Modular in TS
+import * as HeroClasses from "./classes/Hero";
+import powers from "./data/powers";
+
+const superman = new HeroClasses.Hero('Superman',345676,40)
+console.log(superman);
+console.log(superman.power);
+
+
+
+
+
+
+
